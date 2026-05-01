206e6d13a2a35393048f43d1931ad3af421e3ee1
Yusuf Tegar Nababan <yusuuufnababan@gmail.com>
2026-05-01 21:49:35 +0700

    Update Latihan6.java

--- P3_Yusuf Tegar Nababan/src/Latihan/Latihan6.java
+++ P3_Yusuf Tegar Nababan/src/Latihan/Latihan6.java
@@ -1,46 +1,39 @@
 package Latihan;
 
-import java.text.*;
-
+import java.text.NumberFormat; // Impor spesifik agar lebih rapi
 
 public class Latihan6 {
     public static void main(String [] args) {
-        double angka = 83243463.342245;
-        double angkaPecahan = 0.902235643;
+        double nilaiEksperimen = 95230.754821;
+        double rasioKeberhasilan = 0.8845672;
         
-        NumberFormat NumberFormatter = NumberFormat.getNumberInstance(); 
-        NumberFormat CurrFormatter = NumberFormat.getCurrencyInstance();
-        NumberFormat PercentFormatter = NumberFormat.getPercentInstance();
+        NumberFormat fmtNumerik = NumberFormat.getNumberInstance(); 
+        NumberFormat fmtMataUang = NumberFormat.getCurrencyInstance();
+        NumberFormat fmtPersentase = NumberFormat.getPercentInstance();
         
-        String NumberStr = NumberFormatter.format(angka);
-        String CurrStr = CurrFormatter.format (angka);
-        String PercentStr = PercentFormatter.format (angkaPecahan);
+        System.out.println("--- LOG DATA LABORATORIUM (DEFAULT) ---");
+        System.out.println("Nilai Uji   : " + fmtNumerik.format(nilaiEksperimen));
+        System.out.println("Biaya Riset : " + fmtMataUang.format(nilaiEksperimen));
+        System.out.println("Rasio Sukses: " + fmtPersentase.format(rasioKeberhasilan));
         
-        System.out.println("double Angka = " + angka + " berformat number : " + NumberStr);
-        System.out.println("double Angka = " + angka + " berformat number : " + CurrStr);
-        System.out.println("double Angka = " + angka + " berformat number : " + PercentStr);
+        System.out.println("\n--- PENYESUAIAN PRESISI DATA (CUSTOM) ---");
         
-        System.out.println();
-        System.out.println();
+        fmtNumerik.setMaximumIntegerDigits(8);
+        fmtMataUang.setMaximumIntegerDigits(12);
+        fmtPersentase.setMinimumIntegerDigits(2);
         
-        NumberFormatter.setMaximumIntegerDigits(10);
-        CurrFormatter.setMaximumIntegerDigits(10);
-        PercentFormatter.setMaximumIntegerDigits(10);
+        fmtNumerik.setMinimumFractionDigits(3);
+        fmtNumerik.setMaximumFractionDigits(4);
         
-        NumberFormatter.setMinimumIntegerDigits(1);
-        CurrFormatter.setMinimumIntegerDigits(1);
-        PercentFormatter.setMinimumIntegerDigits(1);
+        fmtMataUang.setMinimumFractionDigits(0);
+        fmtMataUang.setMaximumFractionDigits(1);
         
-        NumberFormatter.setMaximumFractionDigits(5);
-        CurrFormatter.setMaximumFractionDigits(2);
-        PercentFormatter.setMaximumFractionDigits(4);
+        fmtPersentase.setMinimumFractionDigits(5);
+        fmtPersentase.setMaximumFractionDigits(7);
         
-        NumberFormatter.setMinimumFractionDigits(2);
-        CurrFormatter.setMinimumFractionDigits(2);
-        PercentFormatter.setMinimumFractionDigits(6);
-        
-        System.out.println("double Angka = " + angka + " berformat number : " + NumberFormatter.format(angka));
-        System.out.println("double Angka = " + angka + " berformat currency : " + CurrFormatter.format(angka));
-        System.out.println("double Angka = " + angka + " berformat percent : " + PercentFormatter.format(angkaPecahan));
+        // Output Hasil Akhir
+        System.out.println("Format Numerik (3-4 desimal) : " + fmtNumerik.format(nilaiEksperimen));
+        System.out.println("Format Moneter (0-1 desimal) : " + fmtMataUang.format(nilaiEksperimen));
+        System.out.println("Format Akurasi Persen        : " + fmtPersentase.format(rasioKeberhasilan));
     }
 }
